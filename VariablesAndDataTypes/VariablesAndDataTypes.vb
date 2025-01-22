a5a6e105b0764387addf06588fee9729d883f658
Jason Permann <JaceFoxman@gmail.com>
2025-01-22 08:51:16 -0700

    Add DIM, and add Integer values

--- VariablesAndDataTypes/VariablesAndDataTypes.vb
+++ VariablesAndDataTypes/VariablesAndDataTypes.vb
@@ -3,9 +3,30 @@
 'RCET2265
 'OperatorsExample
 'https://github.com/JaceFoxman/OperatorsExample.git
+
+Option Explicit On
+
 Module VariablesAndDataTypes
 
     Sub Main()
+
+        'camelCase/Declare Variavbles 
+        Dim jellyBeans As Integer
+        Dim laffyTaffy As Integer
+
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = 120
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = jellyBeans + 10 'can also write as jellyBeans += 10
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans += 10
+        Console.WriteLine(jellyBeans)
+
+        laffyTaffy = 26
+        Console.WriteLine(laffyTaffy)
 
     End Sub
 
