13b6422aef372f4532568153b3f2a13f98d5fec3
Jason Permann <JaceFoxman@gmail.com>
2025-01-22 08:18:16 -0700

    Add project files.

---
+++ VariablesAndDataTypes/VariablesAndDataTypes.vb
@@ -0,0 +1,12 @@
+﻿'Jason Permann
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'https://github.com/JaceFoxman/OperatorsExample.git
+Module VariablesAndDataTypes
+
+    Sub Main()
+
+    End Sub
+
+End Module
